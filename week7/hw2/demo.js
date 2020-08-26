5f0744a9d4ea8cd75409adbe935364a16dffa7c0
Menghua <zoeaeen13@gmail.com>
2020-08-26 11:51:44 +0800

    update week7

--- week7/hw2/demo.js
+++ week7/hw2/demo.js
@@ -1,7 +1,7 @@
 const windows = document.querySelector('html');
 windows.addEventListener('click', (e) => {
   if (e.target.classList.value.includes('faq')) {
-    const answer = e.target.closest('.faqItem').querySelector('.faqAnswer');
-    answer.classList.toggle('hideAnswer');
+    const answer = e.target.closest('.faq-item').querySelector('.faq-answer');
+    answer.classList.toggle('hide-answer');
   }
 });
